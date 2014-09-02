64ef902859e3e6aefc4beb627e8fbfc1af041281
Jacob Rosenthal <jakerosenthal@gmail.com>
2014-09-02 01:21:31 -0700

    updated board defines

--- Boards.h
+++ Boards.h
@@ -126,19 +126,19 @@ writePort(port, value, bitmask):  Write an 8 bit port.
 
 // Spark
 #if defined(SPARK)
-#define TOTAL_ANALOG_PINS       8
+// #define TOTAL_PINS              21 //defined in spark_wiring.h
+// #define TOTAL_ANALOG_PINS       8 //defined in spark_wiring.h
 #define VERSION_BLINK_PIN       7
-#define IS_PIN_DIGITAL(p)       (((p) >= 0 && (p) <= 7) || ((p) >= 10 && (p) <= 17))
-#define IS_PIN_ANALOG(p)        ((p) >= 10 && (p) <= 17)
-#define IS_PIN_PWM(p)           digitalPinHasPWM(p)
-#define IS_PIN_SERVO(p)         ((p) >= 0 && (p) < MAX_SERVOS) //??
+#define IS_PIN_DIGITAL(p)       ((p) >= 0 && (p) < TOTAL_PINS)
+#define IS_PIN_ANALOG(p)        ((p) >= FIRST_ANALOG_PIN && (p) < (FIRST_ANALOG_PIN+TOTAL_ANALOG_PINS))
+#define IS_PIN_PWM(p)           ((p) == 0 || (p) == 1 || (p) == 10 || (p) == 11 || (p) == 14 || (p) == 15 || (p) == 16 || (p) == 17)
+#define IS_PIN_SERVO(p)         IS_PIN_PWM(p)
 #define IS_PIN_I2C(p)           ((p) == 0 || (p) == 1)
 #define IS_PIN_SPI(p)           ((p) == SS || (p) == MOSI || (p) == MISO || (p) == SCK)
 #define PIN_TO_DIGITAL(p)       (p)
-#define PIN_TO_ANALOG(p)        (p)-10
-#define PIN_TO_PWM(p)           PIN_TO_DIGITAL(p)
-#define PIN_TO_SERVO(p)         (p)
-#define IS_PIN_DISABLED(p)      ((p) == 8 || (p) == 9)
+#define PIN_TO_ANALOG(p)        ((p) - FIRST_ANALOG_PIN)
+#define PIN_TO_PWM(p)           (p)
+#define PIN_TO_SERVO(p)         (p) 
 #endif
 
 
